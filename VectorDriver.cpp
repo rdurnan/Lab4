eb21f1f0b58e5eed8d16897f0d1d921c38014b14
rdurnan <43156724+rdurnan@users.noreply.github.com>
2018-10-12 23:45:54 -0600

    Add files via upload

---
+++ VectorDriver.cpp
@@ -0,0 +1,90 @@
+// Lab4.cpp
+// Ryan Durnan
+// COSC-2030-01
+// Lab4
+// 08 October 2018
+
+#include "stdafx.h"
+#include "VectorDriver.h"
+#include <algorithm>
+
+int main() {
+  cout << "  ..:: B E G I N  S A M P L E  C O D E  ::.." << endl << endl;
+  vector<short> sample_vector(5);
+
+  cout << "new vector: ";
+  print(sample_vector);
+  cout << endl;
+
+  fill_vector(sample_vector);
+
+  cout << "filled vector: ";
+  print(sample_vector);
+  cout << endl;
+
+  cout << "sum of vector's elements: " << compute_sum(sample_vector) << endl;
+  cout << endl << "   ..::  E N D   S A M P L E  C O D E  ::.." << endl << endl;
+
+  cout << "\n  ..:: B E G I N  S T U D E N T  C O D E  ::.." << endl << endl;
+
+  vector<short> myV(0);								// Create an empty vector (myV).
+  cout << "New vector (myV): ";
+  print(myV);										// Print out current myV.
+  cout << endl;
+
+  add_numbers(myV);									// Test out add_numbers(myV).
+  cout << "myV after add_numbers(myV): ";
+  print(myV);										// Print out current myV.
+  cout << endl;
+
+  add_numbers(myV);									// Use add_numbers(myV) again to see what happens.
+  cout << "Second add_numbers(myV) call: ";
+  print(myV);										// Print out current myV.
+  cout << endl;
+
+  cout << "Even indexes of myV: ";
+  print_even(myV);									// Test out print_even(myV).
+  cout << endl;
+
+  cout << "Is 7 in myV? ";
+  if (is_present(myV, 7) == true)					// If is_present(myV, 7) returns true...
+  {
+	  cout << "Yes.";								// ...7 is in the vector.
+  }
+  else
+  {
+	  cout << "No.";								// If is_present(myV, 7) returns false, 7 is not in the vector.
+  }
+  cout << endl;
+
+  cout << "Is 2 in myV? ";
+  if (is_present(myV, 2) == true)					// If is_present(myV, 2) returns true...
+  {
+	  cout << "Yes.";								// ...2 is in the vector.
+  }
+  else
+  {
+	  cout << "No.";								// If is_present(myV, 2) returns false, 2 is not in the vector.
+  }
+  cout << endl;
+
+  vector<short> unsorted(0);						// 71-76: Create a new vector (unsorted) and use push_back() to put values in.
+  unsorted.push_back(19);
+  unsorted.push_back(12);
+  unsorted.push_back(5);
+  unsorted.push_back(30);
+  unsorted.push_back(21);
+  cout << "New vector (unsorted): ";
+  print(unsorted);									// Print out current unsorted.
+  cout << endl;
+  sort(unsorted.begin(), unsorted.end());			// Use sort() to put unsorted values in order. 
+  cout << "unsorted after sort(): ";
+  print(unsorted);									// Print out current unsorted.
+  cout << endl;
+
+
+  cout << endl << "   ..::  E N D   S T U D E N T  C O D E  ::.." << endl << endl;
+
+  system("pause");
+  return 0;
+}
---
+++ VectorDriver.h
@@ -0,0 +1,104 @@
+/* File: VectorDriver.cpp
+ * Athr: Dimitri Zarzhitsky
+ * Date: October 16, 2002
+ *
+ * Desc: provides the basic framework and examples for an introduction to
+ *       the STL::vector, iterators, and the algorithms facilities.
+ *
+ * Edited by: Rafe Cooley
+ * Date: October 1, 2017 (the future)
+ *
+ */
+typedef unsigned long ulong;
+
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::vector;
+
+// fill vector that has pre-allocated space with consecutive numbers
+void fill_vector(vector<short> &data) {
+  for (ulong i=0; i<data.size(); i++) {
+    data.at(i) = i;
+  }
+}
+
+// print our vector in a fancy way
+void print(const vector<short> &data) {
+  if (data.empty()) {
+    cout << "<empty>";
+  } else {
+    cout << "<" << data.at(0);
+    for (ulong i=1; i<data.size(); i++) {
+      cout << ", " << data.at(i);
+    }
+    cout << ">";
+  }
+}
+
+// use iterator to sum the elements of a vector
+short compute_sum(const vector<short> &data) {
+  std::vector<short>::const_iterator iter = data.begin();
+
+  short sum = 0;
+  while (iter != data.end()) {
+    sum += *iter;
+    iter++;
+  }
+
+  return sum;
+}
+
+// TODO: implement add_numbers
+void add_numbers(vector<short> &data)
+{
+	for (int i = 0; i <= 9; i++)							// Start a loop that will repeat 10 times.
+	{
+		data.push_back(rand() % 25 + 1);					// Use push_back() to insert random values in the vector.
+	}
+}
+
+// TODO: implement print_even
+void print_even(const vector<short>& data)
+{
+	if (data.empty())										// If the vector is empty...
+	{
+		cout << "<empty>";									// ...report that the vector is empty.
+	}
+	else
+	{														// If the vector is not empty...
+		cout << "<";
+		for (int i = 0; i < data.size(); i++)				// ...start a loop that repeats for every vector element.
+		{
+			if ((i % 2) == 0)								// If the index is even (or 0)...
+			{
+				cout << data.at(i);							// ...print out the value at that index.
+				if ((i + 2) < data.size())					// 78-81: These lines are just for aesthetics. I didn't want a comma to appear on the last element of the vector.
+				{
+					cout << ", ";
+				}
+			}
+		}
+		cout << ">";
+	}
+}
+
+// TODO: implement is_present
+bool is_present(const vector<short> &data, short value)
+{
+	vector<short>::const_iterator iter = data.begin();		// Create the iterator
+	while (iter != data.end())								// While the iterator still points to vector elements...
+	{
+		if (*iter == value)									// ...If the iterator points to a value that is equal to the value we're looking for...
+		{
+			return true;									// ...return true...
+		}
+		iter++;												// ...set the iterator to point at the next vector element.
+	}
+	return false;											// If the value we're looking for was not found in the previous while-loop, return false.
+}
+
+// TODO: implement std::sort function
+// I implemented the sort function in VectorDriver.cpp
